96798c11b0759f2fdddb320ba195fca0cb8e0c49
Tahir Naushasd <t.naushad25@gmail.com>
2017-09-14 10:07:35 +0100

    areas and layout

---
+++ Fiver.Mvc.RazorPages.More/Pages/MyArea2/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fiver.Mvc.RazorPages.More.Pages.MyArea2
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
